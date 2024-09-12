2c4a7a8f56f3ca1dd9c4a69e4db5e13e770dc85f
Dany0504 <a223206520@unison.mx>
2024-09-12 11:43:31 -0700

    Metodo para obtener la union de dos conjuntos

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+
+    public static Set<Integer> unir(Set<Integer> conjunto1, Set<Integer> conjunto2) {
+        Set<Integer> resultado = new HashSet<>(conjunto1);
+        resultado.addAll(conjunto2);
+        return resultado;
+
+    }
+    
+
+}
