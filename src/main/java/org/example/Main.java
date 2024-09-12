1fed44fe61624298bd5ef694517ed9448260aea6
Dany0504 <a223206520@unison.mx>
2024-09-12 11:46:54 -0700

    Metodo para saber si un conjunto es subconjunto de otro

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,9 +20,14 @@ public class Main {
 
 
     public static Set<Integer> diferenciar(Set<Integer> conjunto1, Set<Integer> conjunto2) {
-        Set<Integer> resultado = new HashSet<>(conjunto1); 
+        Set<Integer> resultado = new HashSet<>(conjunto1);
         resultado.removeAll(conjunto2);
         return resultado;
+    }
+
+    // Método para verificar si un conjunto es subconjunto de otro
+    public static boolean esSubconjunto(Set<Integer> conjunto1, Set<Integer> conjunto2) {
+        return conjunto1.containsAll(conjunto2); // Verificar si 'conjunto1' contiene todos los elementos de 'conjunto2'
     }
 
 
